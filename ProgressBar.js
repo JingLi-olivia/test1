d9ede362ce0a8a50c64620394bb50b7a71f1247a
peng-zhou <peterzhou.syd@gmail.com>
2020-06-26 23:29:16 +1000

    test commit

--- ProgressBar.js
+++ ProgressBar.js
@@ -45,7 +45,7 @@ export const ProgressBarContainer = () => {
                 <button onClick={() => setProgress(percentRange >0 ?
                     percentRange - 20 : 0)}>Decrease
                 </button>
-                <button onClick={() => setProgress(percentRange < 100 ? percentRange + 20 : 100)}>Increase</button>
+                <button onClick={() => setProgress(percentRange + 20)}>Increase</button>
                 <button onClick={() => setProgress(0)}>Reset</button>
             </div>
         </div>
