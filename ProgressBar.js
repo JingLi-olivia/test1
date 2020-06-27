f33b22648495f218e3a5a68a6082a251141a2742
JingLi-olivia <jlijing@hotmail.com>
2020-06-26 22:22:22 -0700

    first commit

---
+++ ProgressBar.js
@@ -0,0 +1,42 @@
+import React, {useState} from 'react';
+import './progress-bar.css';
+
+const Range = (props) => {
+    return (
+        <div className="progressbar-value " style={{width: `${props.percentRange}%`}}>
+        </div>
+    );
+};
+
+const ProgressBar = (props) => {
+    return (
+        <div className="progress-bar">
+            <Range percentRange={props.percentRange}/>
+            <span class='progressbar-label'>{Math.round(props.percentRange)}%</span>
+        </div>
+    );
+};
+
+export const ProgressBarContainer = () => {
+    let [percentRange, setProgress] = useState(30);
+    const dropdownlist = ["item1", "item2", "item3"]
+
+    return (
+        <div className="container">
+            <ProgressBar percentRange={percentRange}/>
+
+            <div className="toggle-buttons">
+                <select>
+                    <option value="Luke Skywalker">Luke Skywalker</option>
+                    <option value="C-3PO">C-3PO</option>
+                    <option value="R2-D2">R2-D2</option>
+                </select>
+                <button onClick={() => setProgress(percentRange >0 ?
+                    percentRange - 20 : 0)}>Decrease
+                </button>
+                <button onClick={() => setProgress(percentRange < 100 ? percentRange + 20 : 100)}>Increase</button>
+                <button onClick={() => setProgress(0)}>Reset</button>
+            </div>
+        </div>
+    );
+};
