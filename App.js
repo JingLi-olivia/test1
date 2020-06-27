e2157fd1acc16bc804ae82d84fe218c567931253
Zhou, Peng <Peng.Zhou@news.com.au>
2020-06-27 14:20:46 +1000

    set variables with buttons, bars and limit, retrive data from endpoint.

--- App.js
+++ App.js
@@ -6,15 +6,24 @@ import {ProgressBarContainer} from './ProgressBar';
 
 class App extends Component {
     state = {
-        todos: []
+        buttons: [],
+        bars: [],
+        limit: []
     }
     componentDidMount() {
-        // fetch('http://pb-api.herokuapp.com/bars')
-        fetch('http://jsonplaceholder.typicode.com/todos')
+        fetch('http://pb-api.herokuapp.com/bars')
+        // fetch('http://jsonplaceholder.typicode.com/todos')
         .then(res => res.json())
         .then((data) => {
-        this.setState({ todos: data })
-        console.log(this.state.todos)
+        this.setState(
+            {
+                buttons: data.buttons,
+                bars: data.bars,
+                limit: data.limit
+            })
+        console.log(this.state.buttons)
+        console.log(this.state.bars)
+        console.log(this.state.limit)
         })
         .catch(console.log)
     }
@@ -24,22 +33,10 @@ class App extends Component {
                 {/* <ProgressBarContainer/> */}
 
                 <h1>My Todos</h1>
-                {this.state.todos.map((todo) => (
+                {this.state.buttons.map((button) => (
                 <div className="card">
                     <div className="card-body">
-                    <h5 className="card-title">{todo.title}</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">
-                    { todo.completed &&
-                        <span>
-                        Completed
-                        </span>
-                    }
-                    { !todo.completed &&
-                        <span>
-                        Pending
-                        </span>
-                    }              
-                    </h6>
+                    <h5 className="card-title">{button[0]}</h5>
                     </div>
                 </div>
                 ))}
